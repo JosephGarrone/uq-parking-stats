1c79e2f9618eaa514ef41c57b9972ee17134c611
JosephGarrone <josephgarrone@gmail.com>
2016-03-04 00:08:35 +1000

    Latest

--- routes/index.js
+++ routes/index.js
@@ -22,9 +22,23 @@ router.get('/', function (req, res, next) {
 
 /* GET data */
 router.get('/:carpark/:date', function (req, res, next) {
-    conn.query('SELECT `id`, `available`, `time` FROM `car_park_info` WHERE `car_park` = ? AND DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") GROUP BY UNIX_TIMESTAMP(`time`) DIV 300', [req.params.carpark, req.params.date], function(err, rows, fields) {
-        res.send(rows);
-    });
+    if (req.param.carpark == "all") {
+        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+            res.send(rows);
+        });
+    } else if (req.param.carpark == "casual") {
+        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 1 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+            res.send(rows);
+        });
+    } else if (req.param.carpark == "permits") {
+        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 0 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+            res.send(rows);
+        });
+    } else {
+        conn.query('SELECT `available`, `time` FROM `car_park_info` WHERE `car_park` = ? AND DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") GROUP BY UNIX_TIMESTAMP(`time`) DIV 300;', [req.params.carpark, req.params.date], function(err, rows, fields) {
+            res.send(rows);
+        });
+    }
 })
 
 module.exports = router;

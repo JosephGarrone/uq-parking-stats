db935aa67f22973f3a4fb4613983b70d48a89f63
JosephGarrone <josephgarrone@gmail.com>
2016-03-04 00:11:49 +1000

    Latest

--- routes/index.js
+++ routes/index.js
@@ -23,15 +23,15 @@ router.get('/', function (req, res, next) {
 /* GET data */
 router.get('/:carpark/:date', function (req, res, next) {
     if (req.params.carpark == "all") {
-        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+        conn.query('SELECT SUM(`a`.`available`) AS `available`, `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
             res.send(rows);
         });
     } else if (req.params.carpark == "casual") {
-        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 1 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+        conn.query('SELECT SUM(`a`.`available`) AS `available`, `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 1 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
             res.send(rows);
         });
     } else if (req.params.carpark == "permits") {
-        conn.query('SELECT SUM(`a`.`available`), `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 0 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
+        conn.query('SELECT SUM(`a`.`available`) AS `available`, `a`.`time` FROM (SELECT `available`, `time` FROM `car_park_info` LEFT JOIN `car_parks` ON (`car_parks`.`id` = `car_park_info`.`car_park`) WHERE DATE(CONVERT_TZ(`time`, "+00:00", "+10:00")) = STR_TO_DATE(?, "%Y-%m-%d") AND `car_parks`.`casual` = 0 GROUP BY `car_park`, UNIX_TIMESTAMP(`time`) DIV 300) AS `a` GROUP BY `time`;', [req.params.date], function(err, rows, fields) {
             res.send(rows);
         });
     } else {
